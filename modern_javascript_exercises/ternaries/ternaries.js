6bce6e424b223787d2b4b8f4b5b17683be6fbe1e
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-05-24 12:08:25 +0100

    feat: Implemented Untill spread_syntax

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,7 +1,10 @@
 // Instructions can be found in ternaries.md
 
 export function sayHello(name) {
-  if (name === undefined) {
-    return "Hello you!";
-  } else return "Hello, " + name + "!";
+
+  return !name ? "Hello you!" : "Hello, " + name + "!";
 }
+
+// if (name === undefined) {
+//   return "Hello you!";
+// } else return "Hello, " + name + "!";
