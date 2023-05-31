93a0dcd3553afeb6c666c9b61a6ca36b11a138d8
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-05-31 17:34:04 +0100

    feat: wrote another unit test for template_literal

--- modern_javascript_exercises/template_literals/template_literals.test.js
+++ modern_javascript_exercises/template_literals/template_literals.test.js
@@ -4,3 +4,8 @@ import { greeting } from "./template_literals";
 test('it returns the string "Hello Jimbob! It is now Tuesday"', () => {
   expect(greeting("Jimbob", "Tuesday")).toBe("Hello Jimbob! It is now Tuesday");
 });
+
+test('it return the string "Hello Hitendra! It is now Wednesday"', () => {
+  expect(greeting("Hitendra", "Wednesday")).toBe("Hello Hitendra! It is now Wednesday");
+});
+
